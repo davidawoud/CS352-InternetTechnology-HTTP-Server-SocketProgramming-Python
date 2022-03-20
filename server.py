047f0314f34b5a48f27fe74b4c6631db60b820df
davidawoud <66363752+davidawoud@users.noreply.github.com>
2022-03-20 01:11:50 -0400

    Final without logout

--- server.py
+++ server.py
@@ -63,10 +63,27 @@ signal.signal(signal.SIGINT, sigint_handler)
 
 # TODO: put your application logic here!
 # Read login credentials for all the users
+login_file = open('passwords.txt','r')
+lines = login_file.readlines()
+login_file.close()
+login_info = {}
+for line in lines:
+    v = line.strip().split()[1]
+    k = line.strip().split()[0]
+    login_info[k] = v.strip()
+
 # Read secret data of all the users
+secrets_file = open('secrets.txt','r')
+lines = secrets_file.readlines()
+secrets_file.close()
+secrets_info = {}
+for line in lines:
+    v = line.strip().split()[1]
+    k = line.strip().split()[0]
+    secrets_info[k] = v.strip()
 
-
-
+# cookie data structure
+cookies_info = {}
 
 ### Loop to accept incoming HTTP connections and respond.
 while True:
@@ -82,12 +99,58 @@ while True:
 
     # TODO: Put your application logic here!
     # Parse headers and body and perform various actions
+    input_cookie_token = ""
+    header_lines = headers.split('\r\n')
+    for line in header_lines:
+        header_name = line.split(':')[0]
+        if header_name == "Cookie":
+            input_cookie_token = line.split(':')[1].split('=')[1].strip()
+
+    username = ""
+    password = ""
+    if len(header_body) > 1:
+        input_list = body.split('&')
+        for input in input_list:
+            if input.split('=')[0] == "username":
+                username = input.split('=')[1]
+            if input.split('=')[0] == "password":
+                password = input.split('=')[1]
+    
+    html_content_to_send = ""
+    headers_to_send = ""
+        
+    try:
+        bad_cookie = False
+        good_cookie = False
+        if input_cookie_token != "":
+            if cookies_info.has_key(input_cookie_token):
+                cookie_user = cookies_info[input_cookie_token]
+                html_content_to_send = success_page + secrets_info[cookie_user]
+                good_cookie = True
+            else:
+                bad_cookie = True
+        if not good_cookie:
+            if username == "" and password == "" and bad_cookie:
+                html_content_to_send = bad_creds_page
+            elif username != "" and password == login_info[username]:
+                rand_val = random.getrandbits(64)
+                headers_to_send = 'Set-Cookie: token=' + str(rand_val) + '\r\n'
+                html_content_to_send = success_page + secrets_info[username]
+                cookies_info[str(rand_val)] = username
+            elif (username != "" and password != login_info[username])\
+             or (username == "" and password != "")\
+             or (username != "" and password == ""):
+                html_content_to_send = bad_creds_page
+            else:
+                html_content_to_send = login_page
+    except KeyError as err:
+            html_content_to_send = bad_creds_page
 
     # You need to set the variables:
     # (1) `html_content_to_send` => add the HTML content you'd
     # like to send to the client.
     # Right now, we just send the default login page.
-    html_content_to_send = login_page
+    #html_content_to_send = login_page
     # But other possibilities exist, including
     # html_content_to_send = success_page + <secret>
     # html_content_to_send = bad_creds_page
@@ -96,7 +159,7 @@ while True:
     # (2) `headers_to_send` => add any additional headers
     # you'd like to send the client?
     # Right now, we don't send any extra headers.
-    headers_to_send = ''
+    #headers_to_send = ''
 
     # Construct and send the final response
     response  = 'HTTP/1.1 200 OK\r\n'
